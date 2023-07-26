faaa8a3d8026f8305ca4d6afc61616780dc4f3f9
Ben Sabatier (sabatierb) <bensab62@gmail.com>
2023-07-26 18:45:05 +0100

    Adding images

--- VertoTestProject/Program.cs
+++ VertoTestProject/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the app container
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+//Allow css files to be applied
+app.UseStaticFiles();
+
+//Routes automatically get routed
+app.UseRouting();
+
+//Set up the default route
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}"
+);
+
 
 app.Run();
