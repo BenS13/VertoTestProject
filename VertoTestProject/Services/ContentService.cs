b53fee8b5f8cd0c1023e1cde1e90f818f1fd3603
Ben Sabatier (sabatierb) <bensab62@gmail.com>
2023-07-27 17:39:42 +0100

    Create ContentService to perfom CRUD operations on the database

---
+++ VertoTestProject/Services/ContentService.cs
@@ -0,0 +1,30 @@
+﻿using VertoTestProject.Data;
+using VertoTestProject.Models;
+
+namespace VertoTestProject.Services
+{
+    //Define a Class as a Service that exposes methods to list, add and edit content
+    //GetContent() retrives all content from DB for example
+    //Uses the ContentContext Class to read and write to the DB
+    //Registred for dependecy injection in "Pprogram.cs"
+    public class ContentService
+    {
+        private readonly ContentContext _context = default!;
+
+        //Inject the ContentContext class
+        public ContentService(ContentContext context) 
+        { 
+            _context = context;
+        }
+
+        public IList<Content> GetContent()
+        {
+            if(_context.Contents != null)//If content exists in the database
+            {
+                return _context.Contents.ToList();//Rertrive content from the database
+            }
+            return new List<Content>();//Else return an empty list of type Content
+        }
+
+    }
+}
