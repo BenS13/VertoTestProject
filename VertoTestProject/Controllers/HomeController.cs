277c540493ddfb5746047401effdacc144c363e5
Ben Sabatier (sabatierb) <bensab62@gmail.com>
2023-07-27 18:47:59 +0100

    Content data can now be passed from controller to view

--- VertoTestProject/Controllers/HomeController.cs
+++ VertoTestProject/Controllers/HomeController.cs
@@ -1,15 +1,32 @@
 ﻿//First controller will go here
-using VertoTestProject.Models;
+
 using Microsoft.AspNetCore.Mvc;
+
+using VertoTestProject.Models;
+using VertoTestProject.Services;
 
 
 namespace VertoTestProject.Controllers
 {
     public class HomeController : Controller
     {
+        //Create an instance of the ContentService
+        private readonly ContentService _service;
+
+        //Define a list to store the contents
+        public IList<Content> ContentList { get; set; } = default!;
+
+        //Inject the ContentService into the controller
+        public HomeController(ContentService service)
+        {
+            _service = service;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IList<Content> ContentList = _service.GetContent();
+
+            return View(ContentList);
         }
 
         public IActionResult Cms()
